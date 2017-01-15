3e29dd49e98e23aabcb18ead09269cbd25eee7ef
Stijn Houben <stijn.houben@victhorious.com>
2017-01-15 18:56:39 +0100

    reset static files settings;

--- vbl_calendar/settings.py
+++ vbl_calendar/settings.py
@@ -110,20 +110,11 @@ TEMPLATE_DIRS = (
     (PROJECT_ROOT + '/templates'),
 )
 
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, '../vblcalendar/static/'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, '../vblcalendar/static/'),
+)
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, '../vblcalendar/static/src/img/')
 MEDIA_URL = '/img/'
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
