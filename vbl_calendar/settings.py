4c11964792385ea239746acfc0d6e69c0c8a912a
Stijn Houben <stijn.houben@victhorious.com>
2017-01-16 00:11:31 +0100

    add bootstrap and jquery cdn

--- vbl_calendar/settings.py
+++ vbl_calendar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%^tq)e&c3(*!o#=0b^(vu*v9xq*21fq@21=-l#il)_f&kj7rr='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
