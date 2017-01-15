7136440bb3a60697994f98d0d271cb72728896c2
Stijn Houben <stijn.houben@victhorious.com>
2017-01-15 23:55:51 +0100

    test heroku settings

--- vbl_calendar/settings.py
+++ vbl_calendar/settings.py
@@ -97,20 +97,20 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
-
-ROOT_URLCONF = 'vbl_calendar.urls'
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+ROOT_URLCONF = 'vbl_calendar.urls'
 
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, '../vblcalendar/static/src/img/')
 MEDIA_URL = '/img/'
